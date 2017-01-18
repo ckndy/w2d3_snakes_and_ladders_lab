c75ff4f929a28c636643201f06b1304fc7820d2c
Chris Kennedy <chris.kennedy@live.co.uk>
2017-01-18 19:00:08 +0000

    Update board_spec.rb

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -1,27 +1,31 @@
-require('minitest/autorun')
-require('minitest/rg')
-require_relative('../board')
-require_relative('../player.rb')
+require 'minitest/autorun'
+require 'minitest/rg'
 
+require_relative '../board'
 
-class TestBoard < MiniTest::Test
-
+class TestBoard < Minitest::Test
   def setup
-
-    @player1 = Player.new("Chris")
-    @player2 = Player.new("Ryan")
-    @board = Board.new([@player1, @player2])
+    positions = {
+      2 => 4,
+      7 => -7,
+    }
+    @board = Board.new(9, positions)
   end
 
-  #def test_board_name
-    #assert_equal("Snakes and Ladders", @board.name)
-  #end
-
-  # def test_board_squares
-  #   assert_equal(30, @board.number_of_squares)
-  # end
-
-  def test_initial_state
-    assert_equal(2, @board.number_of_players)
+  def test_board_should_have_9_tiles
+    assert_equal(9, @board.number_of_tiles)
   end
+
+  def test_position_2_is_a_ladder
+    assert_equal(4, @board.modifier_at_position(2))
+  end
+
+  def test_position_7_is_a_snake
+    assert_equal(-7, @board.modifier_at_position(7))
+  end
+
+  def test_win_tile
+    assert_equal(8, @board.win_tile)
+  end
+
 end
